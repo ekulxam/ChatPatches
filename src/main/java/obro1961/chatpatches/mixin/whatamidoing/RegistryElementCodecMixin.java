afd3d9e38df884090df115bbd446cbb67b276ec0
SkyNotTheLimit <159592458+ekulxam@users.noreply.github.com>
2026-01-04 23:46:28 -0800

    Second mixin go

--- src/main/java/obro1961/chatpatches/mixin/whatamidoing/RegistryElementCodecMixin.java
+++ src/main/java/obro1961/chatpatches/mixin/whatamidoing/RegistryElementCodecMixin.java
@@ -1,13 +1,13 @@
 package obro1961.chatpatches.mixin.whatamidoing;
 
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
-import net.minecraft.registry.entry.RegistryFixedCodec;
+import net.minecraft.registry.entry.RegistryElementCodec;
 import obro1961.chatpatches.ChatPatches;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 
-@Mixin(RegistryFixedCodec.class)
-public class RegistryFixedCodecMixin {
+@Mixin(RegistryElementCodec.class)
+public class RegistryElementCodecMixin {
 
     @ModifyExpressionValue(method = "encode(Lnet/minecraft/registry/entry/RegistryEntry;Lcom/mojang/serialization/DynamicOps;Ljava/lang/Object;)Lcom/mojang/serialization/DataResult;", at = @At(value = "INVOKE", target = "Lnet/minecraft/registry/entry/RegistryEntry;ownerEquals(Lnet/minecraft/registry/entry/RegistryEntryOwner;)Z"))
     private boolean forceSameRegistry(boolean original) {
